0c5964136841a2ecced9381cf8ac90f797a15854
Chris Dawes <cmsd2@cantab.net>
2016-12-28 01:07:09 +0000

    add dummy _Unwind_Resume and abort on panic

--- src/lib.rs
+++ src/lib.rs
@@ -84,8 +84,15 @@ extern "C" fn eh_personality() {}
 
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
+#[no_mangle]
 extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
     println!("\n\nPANIC in {} at line {}:", file, line);
     println!("    {}", fmt);
+    loop {}
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
     loop {}
 }
